d8a64403b64bfbb7c4b07707d0e36b78fdeff4bf
alan <alan@wecancodeit.org>
2021-01-13 22:17:29 -0500

    add web layer tests

--- src/test/java/com/example/SpringDemo/ComputerWebLayerTest.java
+++ src/test/java/com/example/SpringDemo/ComputerWebLayerTest.java
@@ -1,5 +1,6 @@
 package com.example.SpringDemo;
 
+import static org.hamcrest.Matchers.is;
 import static org.mockito.Mockito.when;
 
 import java.util.Arrays;
@@ -12,10 +13,11 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(ComputerController.class)
-public class WebLayerTest {
+public class ComputerWebLayerTest {
 
     @Autowired
     private MockMvc mockMvc;
@@ -40,6 +42,12 @@ public class WebLayerTest {
     public void shouldReturnOkWhenFindingComp1() throws Exception {
         when(computerRepo.getOneComputerByName(comp1Name)).thenReturn(comp1);
         mockMvc.perform(get("/computers/mac")).andExpect(status().isOk());
+    }
+
+    @Test
+    public void shouldAddComp1ToModel() throws Exception {
+        when(computerRepo.getOneComputerByName(comp1Name)).thenReturn(comp1);
+        mockMvc.perform(get("/computers/mac")).andExpect(model().attribute("singleComputerModel",is(comp1)));
     }
 
 }
